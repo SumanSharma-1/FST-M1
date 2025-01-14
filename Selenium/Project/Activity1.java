2dc1efbdbfb8cc7f3802d44a48b1e9ee050e4d7d
SumanSharma-1 <suman.sharma@in.ibm.com>
2025-01-14 16:25:40 +0530

    Add files via upload

---
+++ Selenium/Project/Activity1.java
@@ -0,0 +1,29 @@
+package project;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Activity1 {
+	WebDriver driver;
+	
+	@BeforeClass
+	public void setup() {
+		driver= new FirefoxDriver();
+		driver.navigate().to("https://alchemy.hguy.co/jobs/");
+	}
+	
+	@Test
+	public void validateTitle() {
+		Assert.assertEquals(driver.getTitle(), "Alchemy Jobs – Job Board Application");
+	}
+	
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
+
+}
